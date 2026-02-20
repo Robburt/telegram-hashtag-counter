7d451a82e8af9a14bce26c8aee82d194cc643692
Robburt <robgum55@gmail.com>
2026-02-20 09:07:59 +0300

    Moved Table to other folder

--- hashtag_counter.py
+++ hashtag_counter.py
@@ -1,46 +1,8 @@
 import os
 import json
-import xlsxwriter
+import modules.Table
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
-
-
-class Table:
-    def __init__(self):
-        self.save_dir = "results.xlsx"
-        self.workbook = xlsxwriter.Workbook(self.save_dir)
-        self.worksheet = self.workbook.add_worksheet()
-        self.next_free_column = 0
-        self.offset = 1
-
-    def print_dict(self, dictionary, title1, title2):
-        title_column_width = max([len(key) for key in dictionary.keys()])
-        self.worksheet.set_column(self.next_free_column, self.next_free_column, title_column_width)
-        self.worksheet.write(0, self.next_free_column, title1)
-        self.worksheet.write(0, self.next_free_column + 1, title2)
-        for row, key, value in zip(range(len(dictionary.keys())), dictionary.keys(), dictionary.values()):
-            if type(value) is Tag:
-                value = value.uses_amount
-            self.worksheet.write(row + self.offset, self.next_free_column, key)
-            self.worksheet.write(row + self.offset, self.next_free_column + 1, value)
-        self.next_free_column += 2
-
-    def print_groups(self, dictionary):
-        groups = {}
-        with open("groups.txt") as f:
-            for line in f:
-                group_name, tags_in_group = line.split(": ")
-                groups[group_name] = tags_in_group.split()
-        for group_name, tag_list in groups.items():
-            print_query = {}
-            for tag, value in dictionary.items():
-                if tag in tag_list:
-                    print_query[tag] = value
-            if len(print_query.keys()) > 0:
-                self.print_dict(print_query, group_name, '')
-
-    def close_workbook(self):
-        self.workbook.close()
 
 class WindowInterface:
     def __init__(self):
@@ -317,7 +279,7 @@ class Counter:
             "Tag uses total": sum(i.uses_amount for i in self.artists_table.values())
         }
 
-        table = Table()
+        table = modules.Table.Table()
         table.print_dict(self.tags_table, 'Tag', 'Tag uses')
         table.print_dict({key: value for key, value in sorted(self.tags_table.items())}, 'Alphabetic tags', 'Tag uses')
         table.print_dict(additional_information, '', '')
---
+++ modules/Table.py
@@ -0,0 +1,42 @@
+import xlsxwriter
+import hashtag_counter
+
+class Table:
+    def __init__(self):
+        self.save_dir = "results.xlsx"
+        self.workbook = xlsxwriter.Workbook(self.save_dir)
+        self.worksheet = self.workbook.add_worksheet()
+        self.next_free_column = 0
+        self.offset = 1
+
+
+    def print_dict(self, dictionary, title1, title2):
+        title_column_width = max([len(key) for key in dictionary.keys()])
+        self.worksheet.set_column(self.next_free_column, self.next_free_column, title_column_width)
+        self.worksheet.write(0, self.next_free_column, title1)
+        self.worksheet.write(0, self.next_free_column + 1, title2)
+        for row, key, value in zip(range(len(dictionary.keys())), dictionary.keys(), dictionary.values()):
+            if type(value) is hashtag_counter.Tag:
+                value = value.uses_amount
+            self.worksheet.write(row + self.offset, self.next_free_column, key)
+            self.worksheet.write(row + self.offset, self.next_free_column + 1, value)
+        self.next_free_column += 2
+
+
+    def print_groups(self, dictionary):
+        groups = {}
+        with open("groups.txt") as f:
+            for line in f:
+                group_name, tags_in_group = line.split(": ")
+                groups[group_name] = tags_in_group.split()
+        for group_name, tag_list in groups.items():
+            print_query = {}
+            for tag, value in dictionary.items():
+                if tag in tag_list:
+                    print_query[tag] = value
+            if len(print_query.keys()) > 0:
+                self.print_dict(print_query, group_name, '')
+
+
+    def close_workbook(self):
+        self.workbook.close()
